c9f3735c021a5e33a0c801f98f30daec44faa0ed
timur608 <timur1608@list.ru>
2025-03-27 19:16:44 +0300

    add cartserver, clientserver and notificationserver

---
+++ notificationserver/src/test/java/shopping/notificationserver/NotificationserverApplicationTests.java
@@ -0,0 +1,13 @@
+package shopping.notificationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
